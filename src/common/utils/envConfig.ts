307d5ffb42c5074f0917a52b05759a2a4317fc42
Thuan Le <lebathuantpm@gmail.com>
2024-03-16 07:49:37 +0700

    chore: set default value

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -4,10 +4,10 @@ import { cleanEnv, host, num, port, str } from 'envalid';
 dotenv.config();
 
 export const env = cleanEnv(process.env, {
-  NODE_ENV: str({ choices: ['development', 'test', 'production'] }),
-  HOST: host(),
-  PORT: port(),
-  CORS_ORIGIN: str(),
-  COMMON_RATE_LIMIT_MAX_REQUESTS: num(),
-  COMMON_RATE_LIMIT_WINDOW_MS: num(),
+  NODE_ENV: str({ choices: ['development', 'test', 'production'], default: 'production' }),
+  HOST: host({ default: 'localhost' }),
+  PORT: port({ default: 3000 }),
+  CORS_ORIGIN: str({ default: '*' }),
+  COMMON_RATE_LIMIT_MAX_REQUESTS: num({ default: 100 }),
+  COMMON_RATE_LIMIT_WINDOW_MS: num({ default: 60000 }),
 });
